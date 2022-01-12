3de34a3093bf62dbe4cea24e72d48b7d5c327fd1
ViniciusGomesBrandao <macarraojovial@gmail.com>
2022-01-12 16:40:10 -0300

    Update

--- __init__.py
+++ __init__.py
@@ -1,24 +1,25 @@
 
 from flask import Flask
 from flask_restful import Api, Resource
-from flask_socketio import SocketIO, emit, namespace
-from balance import Balance
-import time
-
-
+from flask_socketio import SocketIO, emit
+from threading import Thread
+from time import sleep
+import requests
 app = Flask(__name__)
 api = Api(app)
-
 socketio = SocketIO(app, cors_allowed_origins="*")
-socketio.on_namespace(Balance('/balance'))
 class Index(Resource):
     def get(self):
         socketio.emit('event', {"balance": 2}, namespace="/balance")
         return {'success': True}
 api.add_resource(Index, '/')
-if __name__ == '__main__':
-    
+def startServer():
     socketio.run(app,host='127.0.0.1', port=8080)
-    
-
-
+def loopRequest():
+    sleep(4)
+    while True:
+        sleep(1)
+        requests.get("http://127.0.0.1:8080")
+if __name__ == '__main__':
+    Thread(target = startServer).start()
+    Thread(target = loopRequest).start()
