f7f95814ff921e7724cbd57bdd877df0ab02e91b
ViniciusGomesBrandao <macarraojovial@gmail.com>
2022-01-12 16:21:47 -0300

    Update

--- __init__.py
+++ __init__.py
@@ -1,19 +1,24 @@
+
 from flask import Flask
-from flask_restful import Api
-from flask_socketio import SocketIO, emit
+from flask_restful import Api, Resource
+from flask_socketio import SocketIO, emit, namespace
+from balance import Balance
+import time
+
+
 app = Flask(__name__)
 api = Api(app)
 
 socketio = SocketIO(app, cors_allowed_origins="*")
-@socketio.on('connect', namespace='/balance')
-def balance():
-    print("Emiting Event")
-    emit('event', { 'balance': 1 }, namespace='/balance')
-
-@socketio.on('connect', namespace='/user')
-def user():
-    print("Emiting Event")
-    emit('event', { 'evento': 'user' }, namespace='/user')
+socketio.on_namespace(Balance('/balance'))
+class Index(Resource):
+    def get(self):
+        socketio.emit('event', {"balance": 2}, namespace="/balance")
+        return {'success': True}
+api.add_resource(Index, '/')
 if __name__ == '__main__':
+    
     socketio.run(app,host='127.0.0.1', port=8080)
     
+
+
